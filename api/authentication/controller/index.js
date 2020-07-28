778afcd308c388fc3a9ad18f59dee330454cb87b
Krzysztof Brak <krzlepianka@gmail.com>
2020-07-28 23:58:51 +0200

    added all routes, function for create, get, put, delete clients

--- api/authentication/controller/index.js
+++ api/authentication/controller/index.js
@@ -1,12 +1,17 @@
 const User = require('../../user/model/index');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 exports.signUp = async (req, res) => {
   const { login, password, email } = req.body;
-  const saltRounds = 5;
+  const { BCRYPT_SALTROUND } = process.env;
+  const convertSaltRoundToNmb = parseInt(BCRYPT_SALTROUND);
   try {
-    const hash = await bcrypt.hash(password, saltRounds);
+    const hash = await bcrypt.hash(
+      password,
+      bcrypt.genSaltSync(convertSaltRoundToNmb)
+    );
     const user = await User.create({
       login: login,
       password: hash,
@@ -20,17 +25,14 @@ exports.signUp = async (req, res) => {
     });
   } catch (err) {
     res.status(404).json({
-      message: 'registration faild',
-      error: {
-        err,
-      },
+      message: err.message,
     });
   }
 };
 
 exports.signIn = async (req, res) => {
   const { login, password } = req.body;
-  const saltRounds = 5;
+  const { JWT_SECRET } = process.env;
   let decodedPassword;
   let user;
   try {
@@ -40,10 +42,10 @@ exports.signIn = async (req, res) => {
       if (decodedPassword) {
         const token = jwt.sign(
           {
-            exp: Math.floor(Date.now() / 1000 + 60 * 60),
-            data: user._id,
+            expireTime: Math.floor(Date.now() / 1000 + 60 * 60),
+            id: user._id,
           },
-          'secret'
+          JWT_SECRET
         );
         return res.status(200).json({
           message: 'login succed',
@@ -67,13 +69,14 @@ exports.signIn = async (req, res) => {
 };
 
 exports.checkToken = async (req, res, next) => {
+  const { JWT_SECRET } = process.env;
   try {
     const { authorization } = req.headers;
     const token = authorization.split(' ')[1];
-    const decode = await jwt.verify(token, 'secret');
+    const decode = await jwt.verify(token, JWT_SECRET);
     res.locals.decode = decode;
   } catch (error) {
-    return res.status(200).json({
+    return res.status(404).json({
       message: error.message,
       error,
     });
--- api/client/controller/index.js
+++ api/client/controller/index.js
@@ -2,31 +2,10 @@ const express = require('express');
 const Client = require('../model/index');
 const User = require('../../user/model/index');
 
-exports.createClients = async (req, res) => {
-  try {
-    const { data } = res.locals.decode;
-    const client = await Client.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phone: req.body.phone,
-      email: req.body.email,
-      createdBy: data,
-    });
-    res.status(200).json({
-      message: 'you added a new client',
-      client,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: 'something gone wrong',
-    });
-  }
-};
-
 exports.getClients = async (req, res) => {
   try {
-    const { data } = res.locals.decode;
-    const clients = await Client.find({ createdBy: data });
+    const { id } = res.locals.decode;
+    const clients = await Client.find({ createdBy: id });
     res.status(200).json({
       message: 'clients list',
       clients,
@@ -34,6 +13,77 @@ exports.getClients = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       message: 'something gone wrong',
+      error: err.message,
+    });
+  }
+};
+
+exports.getClient = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const client = await Client.findById(id);
+    res.status(200).json({
+      message: 'you found a client',
+      client,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: 'there is no client with id that you passed',
+      error: error.messae,
+    });
+  }
+};
+
+exports.createClient = async (req, res) => {
+  try {
+    const { id } = res.locals.decode;
+    const client = await Client.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phone: req.body.phone,
+      email: req.body.email,
+      createdBy: id,
+    });
+    res.status(200).json({
+      message: 'you added a new client',
+      client,
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: 'something gone wrong',
+      error: err.message,
+    });
+  }
+};
+
+exports.editClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedClient = await Client.findByIdAndUpdate(id, {
+      firstName: 'Maria',
+    });
+    res.status(200).json({
+      message: 'this rout works',
+      updatedClient,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.deleteClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedClient = await Client.findByIdAndDelete(id);
+    res.status(200).json({
+      message: `you deleted client with id ${id}`,
+      deletedClient,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
     });
   }
 };
